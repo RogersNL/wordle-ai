7f074d12af3c2adbb928e6c6a6f54470f11a2377
Nicholas Rogers <RogersNL@live.com>
2024-08-17 22:56:03 -0700

    Move root folder

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import GameContent from "./components/GameContent";
+
+const App = () => {
+  return (
+    <>
+      <GameContent />
+    </>
+  );
+};
+
+export default App;
---
+++ src/components/GameContent.tsx
@@ -0,0 +1,55 @@
+import { Box, Container, Stack } from "@mui/material";
+import React from "react";
+
+const GameContent = () => {
+  return (
+    <Container>
+      <Stack gap={2}>
+        <Stack gap={2} direction="row">
+          <Box sx={{ border: "2px solid grey", borderRadius: "2px" }}></Box>
+          <Box sx={{ border: "2px solid grey", borderRadius: "2px" }}></Box>
+          <Box sx={{ border: "2px solid grey", borderRadius: "2px" }}></Box>
+          <Box sx={{ border: "2px solid grey", borderRadius: "2px" }}></Box>
+          <Box sx={{ border: "2px solid grey", borderRadius: "2px" }}></Box>
+        </Stack>
+        <Stack gap={2} direction="row">
+          <Box sx={{ border: "2px solid grey", borderRadius: "2px" }}></Box>
+          <Box sx={{ border: "2px solid grey", borderRadius: "2px" }}></Box>
+          <Box sx={{ border: "2px solid grey", borderRadius: "2px" }}></Box>
+          <Box sx={{ border: "2px solid grey", borderRadius: "2px" }}></Box>
+          <Box sx={{ border: "2px solid grey", borderRadius: "2px" }}></Box>
+        </Stack>
+        <Stack gap={2} direction="row">
+          <Box sx={{ border: "2px solid grey", borderRadius: "2px" }}></Box>
+          <Box sx={{ border: "2px solid grey", borderRadius: "2px" }}></Box>
+          <Box sx={{ border: "2px solid grey", borderRadius: "2px" }}></Box>
+          <Box sx={{ border: "2px solid grey", borderRadius: "2px" }}></Box>
+          <Box sx={{ border: "2px solid grey", borderRadius: "2px" }}></Box>
+        </Stack>
+        <Stack gap={2} direction="row">
+          <Box sx={{ border: "2px solid grey", borderRadius: "2px" }}></Box>
+          <Box sx={{ border: "2px solid grey", borderRadius: "2px" }}></Box>
+          <Box sx={{ border: "2px solid grey", borderRadius: "2px" }}></Box>
+          <Box sx={{ border: "2px solid grey", borderRadius: "2px" }}></Box>
+          <Box sx={{ border: "2px solid grey", borderRadius: "2px" }}></Box>
+        </Stack>
+        <Stack gap={2} direction="row">
+          <Box sx={{ border: "2px solid grey", borderRadius: "2px" }}></Box>
+          <Box sx={{ border: "2px solid grey", borderRadius: "2px" }}></Box>
+          <Box sx={{ border: "2px solid grey", borderRadius: "2px" }}></Box>
+          <Box sx={{ border: "2px solid grey", borderRadius: "2px" }}></Box>
+          <Box sx={{ border: "2px solid grey", borderRadius: "2px" }}></Box>
+        </Stack>
+        <Stack gap={2} direction="row">
+          <Box sx={{ border: "2px solid grey", borderRadius: "2px" }}></Box>
+          <Box sx={{ border: "2px solid grey", borderRadius: "2px" }}></Box>
+          <Box sx={{ border: "2px solid grey", borderRadius: "2px" }}></Box>
+          <Box sx={{ border: "2px solid grey", borderRadius: "2px" }}></Box>
+          <Box sx={{ border: "2px solid grey", borderRadius: "2px" }}></Box>
+        </Stack>
+      </Stack>
+    </Container>
+  );
+};
+
+export default GameContent;
